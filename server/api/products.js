6194b0fde981b562d614ceae7a5add41591dac90
David Burke <Burkedm94@gmail.com>
2022-08-04 00:34:33 -0400

    add backend for products

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,15 @@ const router = require('express').Router()
 const { models: { Product }} = require('../db')
 module.exports = router
 
+router.get("/", async (req, res, next) => {
+  try {
+    const products = Product.findAll();
+    res.send(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.get('/:id', async (req, res, next) => {
   try {
     const cart = await Product.findByPk(req.params.id)
