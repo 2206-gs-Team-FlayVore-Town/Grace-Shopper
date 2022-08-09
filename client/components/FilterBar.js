bdc6216aee32492e4d43ff7ec4bde0f11ac0eadf
David Burke <Burkedm94@gmail.com>
2022-08-09 10:52:49 -0400

    add price filter

--- client/components/FilterBar.js
+++ client/components/FilterBar.js
@@ -1,18 +1,44 @@
 import React from "react";
 
 export class FilterBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
 
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            priceArr : ['0,499','500,999','1000,1499','1500,2499','2500,4999','5000,100000000'],
+            price0to5 : true,
+            price5to10 : true,
+            price10to15 : true,
+            price15to25 : true,
+            price25to50 : true,
+            price50up : true,
+        };
+        this.handlePriceSelect=this.handlePriceSelect.bind(this);
+      }
+
+    handlePriceSelect = (arr, evt, bool) => {
+          console.log("handlePriceSelect arrIn: ", arr)
+          if (bool) {
+              arr.push(evt.target.value);
+          } else {
+              arr = arr.filter((e) => e !== evt.target.value);
+          }
+          console.log("handlePriceSelect arrOut", arr)
+          this.setState({priceArr: arr}, () => console.log(this.state.priceArr))
+          this.props.handleFilterPrice(arr)
+      };
+
+    render() {
+        
+        let {priceArr, price0to5, price5to10, price10to15, price15to25, price25to50, price50up} = this.state;
+        console.log("@@@@",price0to5);
     return (
       <div className="filter-bar">
         <h2> Sort by: </h2>
         <select
-          onChange={(evt) => {this.props.selectSortByParam(evt.target.value)}}
+          onChange={(evt) => {
+            this.props.selectSortByParam(evt.target.value);
+          }}
         >
           <option value="Rating (high to low)">Rating (high to low)</option>
           <option value="Rating (low to high)">Rating (low to high)</option>
@@ -23,27 +49,82 @@ export class FilterBar extends React.Component {
         <h3> Price:</h3>
         <div>
           <div className="filter-row">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              value={[0, 499]}
+              checked={price0to5}
+              onChange={(event) => {
+                price0to5 = !price0to5;
+                this.setState({price0to5 : price0to5});
+                this.handlePriceSelect(priceArr,event,price0to5)
+              }}
+            />
             Less than $5.00
           </div>
           <div className="filter-row">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              value={[500, 999]}
+              checked={price5to10}
+              onChange={(event) => {
+                price5to10 = !price5to10;
+                this.setState({price5to10 : price5to10});
+                this.handlePriceSelect(priceArr,event,price5to10)
+              }}
+            />
             $5.00-$9.99
           </div>
           <div className="filter-row">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              value={[1000, 1499]}
+              checked={price10to15}
+              onChange={(event) => {
+                price10to15 = !price10to15;
+                this.setState({price10to15 : price10to15});
+                this.handlePriceSelect(priceArr,event,price10to15)
+              }}
+            />
             $10.00-$14.99
           </div>
           <div className="filter-row">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              value={[1500, 2499]}
+              checked={price15to25}
+              onChange={(event) => {
+                price15to25 = !price15to25;
+                this.setState({price15to25 : price15to25});
+                this.handlePriceSelect(priceArr,event,price15to25)
+              }}
+            />
             $15.00-$24.99
           </div>
           <div className="filter-row">
-            <input type="checkbox" />
-            $24.99-$50.00
+            <input
+              type="checkbox"
+              value={[2500, 4999]}
+              checked={price25to50}
+              onChange={(event) => {
+                price25to50 = !price25to50;
+                this.setState({price25to50 : price25to50});
+                this.handlePriceSelect(priceArr,event,price25to50)
+              }}
+            />
+            $25.00-$49.99
           </div>
           <div className="filter-row">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              value={[5000, 100000000]}
+              checked={price50up}
+              onChange={(event) => {
+                price50up = !price50up;
+                this.setState({price50up : price50up});
+                this.handlePriceSelect(priceArr,event,price50up)
+
+              }}
+            />
             Higher than $50.00
           </div>
         </div>
--- client/components/Home.js
+++ client/components/Home.js
@@ -15,6 +15,7 @@ export class Home extends React.Component {
       filter: "all",
       sortByParam: "Rating (high to low)",
       sortedProducts: [],
+      priceFilter: ['0,499','500,999','1000,1499','1500,2499','2500,4999','5000,100000000'],
     };
   }
 
@@ -26,13 +27,19 @@ export class Home extends React.Component {
     this.setState({sortByParam: sortByParam})
   }
 
+  handleFilterPrice = (priceFilter) => {
+    this.setState({priceFilter: priceFilter});
+    console.log("handleFilterPrice:", priceFilter)
+  }
+
   render() {
     const { products } = this.props;
-    let { filter, sortByParam, sortedProducts } = this.state;
+    let { filter, sortByParam, sortedProducts, priceFilter } = this.state;
     sortedProducts = products.filter(
       (product) => product.category === filter || filter === "all"
     );
     sortedProducts = sortBy(sortByParam, sortedProducts);
+    sortedProducts = filterPrice(priceFilter, sortedProducts);
     const topItems = sortedProducts.slice(0, 4);
     const otherItems = sortedProducts.slice(5);
     return (
@@ -80,7 +87,7 @@ export class Home extends React.Component {
           <div className="spacer" />
         </div>
         <div className="row">
-          <FilterBar selectSortByParam={this.handleSortByParam}/>
+          <FilterBar selectSortByParam={this.handleSortByParam} handleFilterPrice={this.handleFilterPrice}/>
           <div className="products-array">
             <div className="top-row">
               {topItems.map((product) => {
@@ -119,11 +126,22 @@ const sortBy = (param, arr) => {
   }
 };
 
+const filterPrice = (priceFilter, arr) => {
+  return arr.filter(
+    (product) => (
+      (product.price < 500 && priceFilter.includes('0,499') ||
+       product.price > 499 && product.price < 1000 && priceFilter.includes('500,999') ||
+       product.price > 999 &&product.price < 1500 && priceFilter.includes('1000,1499') ||
+       product.price > 1499 &&product.price < 2500 && priceFilter.includes('1500,2499') ||
+       product.price > 2499 &&product.price < 5000 && priceFilter.includes('2500,4999') ||
+       product.price > 4999 && priceFilter.includes('5000,100000000')
+      )
+    ))
+  
+}
+
 const mapState = (state) => {
   return {
-    filter: "all",
-    sortByParam: "Rating (high to low)",
-    sortedProducts: [],
     products: state.multipleProducts,
   };
 };
