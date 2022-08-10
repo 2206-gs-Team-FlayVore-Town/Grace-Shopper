a4a5f3bd9a1f5fa1d60d6dd947ccf5d13cae485e
vicantlop <105176088+vicantlop@users.noreply.github.com>
2022-08-10 07:15:09 -0700

    working users

--- client/components/Users.js
+++ client/components/Users.js
@@ -7,7 +7,6 @@ const Users = props => {
       <h1 className="title">Users</h1>
       <ul className="all-product-page">
           {props.users.map((user, index) => {
-          log(user)
               return <li key = {index} className="column" id="user">
                 <div>Name: {user.firstName} {user.lastName}</div>
                 <div>Email: {user.email}</div>
